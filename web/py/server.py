839f8d7c039788bb28d9aa016566a0e64ff6b51f
David Grayson <davidegrayson@gmail.com>
2010-05-08 22:10:27 +0000

    Got the websocket stuff to work with a sender thread
    git-svn-id: svn+ssh://ssh.phx.nearlyfreespeech.net/home/private/svn/speedyscrabble@13 e6ed7d64-272f-dd11-a863-0030487297e6

--- web/py/server.py
+++ web/py/server.py
@@ -3,7 +3,7 @@
 Speedy scrabble server
 """
 
-# Pytho 3.1 documentation: http://docs.python.org/py3k/library/index.html
+# Python 3.1 documentation: http://docs.python.org/py3k/library/index.html
 
 from datetime import datetime
 import socketserver
@@ -15,6 +15,11 @@ import socket
 import sys
 import time
 import threading
+import queue
+
+#### Options ####
+_HOST = "speedyscrabble.local"
+_PORT = 83
 
 #### Set up logging ####
 log = logging.getLogger()
@@ -26,10 +31,43 @@ log.addHandler(handler)
 
 log.info("Starting server.")
 
+
+class WebSocket:
+    def __init__(self, socket):
+        self._socket = socket
+        self._send_queue = queue.Queue()
+        self._receive_queue = queue.Queue()
+        self._sender = threading.Thread(target=self.send_thread)
+        self._sender.daemon = True
+        self._sender.start()
+        # TODO: self.receiver = threading.Thread(...)
+
+    def send(self, frame):
+        '''Non-blocking function that puts the frame in a queue to be sent later by the sending thread.'''
+        if frame.__class__ == bytes:
+            # Assumption: if frame is a list of bytes, it is a properly formatting frame already!
+            bytez = frame
+        else:
+            bytez = bytes([0]) + frame.encode('utf-8') + bytes([0xFF])
+        
+        self._send_queue.put(bytez)
+
+    def send_thread(self):
+        while True:
+            bytes = self._send_queue.get()
+            self._socket.sendall(bytes)
+
 class WebSocketServer(socketserver.ThreadingMixIn, http.server.HTTPServer):
     # The threads created will be daemons, which means that the process
     # will terminate even if those threads are still alive.
     socketserver.ThreadingMixIn.daemon_threads = True
+
+    def __init__(self, server_address, RequestHandlerClass, bind_and_activate=True):
+        # Store ws_host and ws_port so they can easily be accessed later (ws_host gets modified
+        # by TCPServer's server_bind function, so we can't just read server_address at a later time).
+        self.ws_host = server_address[0]
+        self.ws_port = server_address[1]
+        http.server.HTTPServer.__init__(self, server_address, RequestHandlerClass, bind_and_activate)
 
 _myvar = 0
 
@@ -42,20 +80,29 @@ class WebSocketHandler(http.server.BaseHTTPRequestHandler):
     #    http://docs.python.org/py3k/library/email.message.html#email.message.Message
 
     def do_GET(self):
-        log.debug("New websocket request, path=%s, headers=%s, origin=%s" % (self.path, self.headers.keys(), self.headers['Origin']))
+        log.debug("New websocket request, path=%s" % self.path)
+        # TODO: set timeout?
+        self.request.settimeout(None)
+        self.request.setblocking(1)
         self.write_line("HTTP/1.1 101 Web Socket Protocol Handshake")
         self.write_line("Upgrade: WebSocket")
         self.write_line("Connection: Upgrade")
-        self.write_line("WebSocket-Origin: http://speedyscrabble.local")
-        self.write_line("WebSocket-Location: ws://speedyscrabble.local:83/play")
+        self.write_line("WebSocket-Origin: http://"+self.server.ws_host)
+        self.write_line("WebSocket-Location: ws://%s:%d/play" % (self.server.ws_host, self.server.ws_port))
         #self.write_line("WebSocket-Protocol: sample")
         self.write_line("")
         self.wfile.flush()
+
+        ws = WebSocket(self.request)
+
         while True:
             time.sleep(1)
-            self.send_message('clock!pid=%d thread=%d myvar=%d %s' % (os.getpid(), threading.current_thread().ident, _myvar, datetime.now()))
+            message = 'clock!pid=%d thread=%d myvar=%d %s' % (os.getpid(), threading.current_thread().ident, _myvar, datetime.now())
+            # self.send_message(message) # tmphax
+            ws.send(message);
 
     def write_line(self, string):
+        log.debug("Sending line: " + string)
         self.wfile.write((string+'\r\n').encode('utf-8'))
 
     def send_message(self, message):
@@ -65,5 +112,5 @@ class WebSocketHandler(http.server.BaseHTTPRequestHandler):
         return
 
 if __name__ == "__main__":
-    server = WebSocketServer(("speedyscrabble.local", 83), WebSocketHandler)
+    server = WebSocketServer((_HOST, _PORT), WebSocketHandler)
     server.serve_forever()
